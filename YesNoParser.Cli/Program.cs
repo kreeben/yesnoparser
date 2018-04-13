7af72194e174ad7655b1770e8d9d265c9b7e2f3d
Marcus Lager <marcus.lager@sembo.se>
2018-04-13 17:51:41 +0200

    first commit

---
+++ YesNoParser.Cli/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace YesNoParser.Cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("missing parameter: path_to_resource (web or local)");
+                Console.WriteLine("press any key to quit");
+                Console.ReadKey();
+                return;
+            }
+            var path = args[0];
+            var text = GetString(path);
+            var decoded = WebUtility.HtmlDecode(text);
+            var parser = new YesNoParser('>', '<');
+            var parsed = parser.Parse(decoded);
+            var fileName = GetSafeFileName(path) + ".txt";
+            File.WriteAllText(fileName, parsed);
+            Console.WriteLine(fileName);
+            Console.WriteLine("press any key to quit");
+            Console.ReadKey();
+        }
+
+        static string GetSafeFileName(string path)
+        {
+            foreach (var c in Path.GetInvalidFileNameChars())
+            {
+                path = path.Replace(c, '-');
+            }
+            return path;
+        }
+
+        static string GetString(string path)
+        {
+            if (path.StartsWith("http://") || path.StartsWith("https://"))
+            {
+                return GetWebString(path);
+            }
+            else
+            {
+                return GetLocalString(path);
+            }
+        }
+
+        static string GetWebString(string url)
+        {
+            var webRequest = WebRequest.Create(url);
+            using (var response = webRequest.GetResponse())
+            using (var content = response.GetResponseStream())
+            using (var reader = new StreamReader(content))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+
+        static string GetLocalString(string fileName)
+        {
+            return File.ReadAllText(fileName);
+        }
+    }
+}
---
+++ src/YesNoParser/YesNoParser.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+
+namespace YesNoParser
+{
+    /// <summary>
+    /// Compiles a string that contains everything from a piece of text that comes after a 
+    /// "yes character" while excluding everything that comes after a "no character".
+    /// Example use case to extract text from a HTML document:
+    /// var parser = new YesNoParser('>', '<');
+    /// var freeFromHtmlTags = parser.Parse(html);
+    /// </summary>
+    public class YesNoParser
+    {
+        private readonly char _yesChar;
+        private readonly char _noChar;
+
+        public YesNoParser(char yesChar, char noChar)
+        {
+            _yesChar = yesChar;
+            _noChar = noChar;
+        }
+
+        public string Parse(string text)
+        {
+            var skipChars = false;
+            var output = new StringBuilder();
+            foreach(var c in text)
+            {
+                if (c == _noChar)
+                {
+                    skipChars = true;
+                }
+                else if (c == _yesChar)
+                {
+                    skipChars = false;
+                }
+                else
+                {
+                    if (!skipChars)
+                    {
+                        output.Append(c);
+                    }
+                }
+            }
+            return output.ToString();
+        }
+    }
+}
